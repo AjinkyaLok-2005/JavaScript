e8db40378cda75a42041b0a51ae2b6f0f2e03924
ajinkya947 <lajinkya645@gmail.com>
2024-12-12 00:02:02 +0530

    Un defined and not defined

---
+++ UnDef&NotDef.js
@@ -0,0 +1,17 @@
+console.log(a)
+//before assigning the value undefined
+// b/c we have allocated memory to a
+//undefined is not empty consists of its own memory
+
+var a = 7;
+
+// console.log(x)
+//this will be not defined
+// b/c we have not allocated memory to x
+
+var b;
+console.log(b);
+b = 10;
+console.log(b);
+b = "Hello";
+console.log(b);
