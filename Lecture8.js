6ae079f4a0bb8e1755c9668d544443ee44b3e570
ajinkya947 <lajinkya645@gmail.com>
2024-12-16 12:16:36 +0530

    let and const

---
+++ Lecture8.js
@@ -0,0 +1,7 @@
+let a = 10;
+//this will have a memory assigned but it will be in other memory space 
+//rather than the global memory space
+//AND we can not access this memory space untill we put some value in them
+
+console.log(a);
+var b = 100;
